a045ee034bb9a41679886918d7d04539238802aa
44hassan <68564400+44hassan@users.noreply.github.com>
2023-05-01 18:53:43 -0700

    Update About.java

--- src/model/About.java
+++ src/model/About.java
@@ -1,5 +1,36 @@
 package model;
 
 public class About {
+    private String versionNumber;
+    private String userName;
+    private String teamName;
+    private ArrayList<Developer> teamMembers;    
     
+    public About(String userName, String teamName, ArrayList<Developer> teamMembers,
+            String versionNumber) {
+        this.userName = userName;
+        this.teamName = teamName;
+        this.teamMembers = new ArrayList<>(teamMembers);
+        this.versionNumber = versionNumber;
+    }
+    
+    public String getUsername() {
+        return userName;
+    }
+    
+    public String getTeamName() {
+        return teamName;
+    }
+    
+    public String getVersion() {
+        return versionNumber; 
+    }
+    
+    public ArrayList<Developer> getDevelopers() {
+        return teamMembers;
+    }
+    
+    public void addDeveloper(Developer dev) {
+        teamMembers.add(dev);
+    }
 }
